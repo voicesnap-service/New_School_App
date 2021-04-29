592ed041f88f3f7f10eb0319584dd2c3aeb9b70c
Murugan <murugan@voicesnap.net>
2021-04-29 10:28:46 +0530

    changes added

---
+++ VOICESNAP_MESSENGER_NEW_APP/app/src/main/java/com/vsnapnewschool/voicesnapmessenger/Network/ApiRequestValues.kt
@@ -0,0 +1,17 @@
+package com.vsnapnewschool.voicesnapmessenger.Network
+
+
+@Suppress("DEPRECATION")
+class ApiRequestValues {
+    companion object {
+
+        var LOGIN_TOKEN: String? = "login_token"
+        var MOBILE_NUMBER: String? = "mobile_number"
+        var SCHOOL_ID: String? = "school_id"
+        var STUDENT_ID: String? = "student_id"
+        var CLASS_ID: String? = "class_id"
+        var SECTION_ID: String? = "section_id"
+
+
+    }
+}
--- VOICESNAP_MESSENGER_NEW_APP/app/src/main/java/com/vsnapnewschool/voicesnapmessenger/Network/StudentAPIServices.kt
+++ VOICESNAP_MESSENGER_NEW_APP/app/src/main/java/com/vsnapnewschool/voicesnapmessenger/Network/StudentAPIServices.kt
@@ -6,6 +6,12 @@ import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.vsca.vsnapvoicecollege.Rest.APIClient
 import com.vsnapnewschool.voicesnapmessenger.CallBacks.GetTextMessagesCallBack
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.CLASS_ID
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.LOGIN_TOKEN
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.MOBILE_NUMBER
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.SCHOOL_ID
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.SECTION_ID
+import com.vsnapnewschool.voicesnapmessenger.Network.ApiRequestValues.Companion.STUDENT_ID
 import com.vsnapnewschool.voicesnapmessenger.R
 import com.vsnapnewschool.voicesnapmessenger.ServiceResponseModels.GetTextMessages
 import com.vsnapnewschool.voicesnapmessenger.ServiceResponseModels.StatusMessageResponse
@@ -27,12 +33,12 @@ object StudentAPIServices {
         val ClassID = Util_shared_preferences.getStudentClassID(activity)
         val SectionID = Util_shared_preferences.getStudentSectionID(activity)
         val jsonObject = JsonObject()
-        jsonObject.addProperty("login_token", loginToken)
-        jsonObject.addProperty("mobile_number", mobileNumber)
-        jsonObject.addProperty("school_id", schoolID)
-        jsonObject.addProperty("student_id", StudentID)
-        jsonObject.addProperty("class_id", ClassID)
-        jsonObject.addProperty("section_id", SectionID)
+        jsonObject.addProperty(LOGIN_TOKEN, loginToken)
+        jsonObject.addProperty(MOBILE_NUMBER, mobileNumber)
+        jsonObject.addProperty(SCHOOL_ID, schoolID)
+        jsonObject.addProperty(STUDENT_ID, StudentID)
+        jsonObject.addProperty(CLASS_ID, ClassID)
+        jsonObject.addProperty(SECTION_ID, SectionID)
         Log.d("ParentTextMsgRequest", jsonObject.toString())
         GifLoading.loading(activity, true)
 
@@ -63,7 +69,6 @@ object StudentAPIServices {
                             } else {
                                 callBack.callBackTextMessages_Archive(responseBody)
                             }
-<<<<<<< HEAD
                         } else if (response?.code() == 400 || response?.code() == 500) {
                             val errorResponseBody = Gson().fromJson(
                                 response.errorBody()?.charStream(),
@@ -71,8 +76,7 @@ object StudentAPIServices {
                             )
                             UtilConstants.handleErrorResponse(activity, response.code(), errorResponseBody
                             )
-=======
->>>>>>> f3d2e822fd1d1eb38f24910f8fcc33efc5eda1e0
+
                         } else {
                             UtilConstants.parentCustomFailureAlert(activity, responseBody.message,ApiType)
                         }
